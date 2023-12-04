b8d4e3c14941037b482fddede63b56d0ea2d6fbc
imaulanaazn <i.maulana.azn@gmail.com>
2023-12-04 14:15:47 +0700

    make folder structure and basic bottom navigation

---
+++ app/src/main/java/com/bangkit/turtlify/ui/activities/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.turtlify.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.turtlify.R
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
---
+++ app/src/main/java/com/bangkit/turtlify/ui/activities/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.bangkit.turtlify.ui.activities
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bangkit.turtlify.R
+import com.bangkit.turtlify.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_maps, R.id.navigation_camera,  R.id.navigation_report_turtle, R.id.navigation_history
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
