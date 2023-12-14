b7676be5a34a161819b1d773242e050b3d1f1070
imaulanaazn <i.maulana.azn@gmail.com>
2023-12-14 10:43:19 +0700

    make layout, adapter, viewmodel, repository for list history

--- app/src/main/java/com/bangkit/turtlify/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/bangkit/turtlify/ui/history/HistoryFragment.kt
@@ -1,22 +1,60 @@
 package com.bangkit.turtlify.ui.history
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bangkit.turtlify.R
+import com.bangkit.turtlify.data.database.repository.Turtle
+import com.bangkit.turtlify.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var viewModel: HistoryViewModel
+    private lateinit var binding: FragmentHistoryBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // Initialize RecyclerView from the binding
+        val rvHistories = binding.rvHistories
+        rvHistories.layoutManager = LinearLayoutManager(requireContext())
+        viewModel = ViewModelProvider(this)[HistoryViewModel::class.java]
+
+        viewModel.getHistories()
+        viewModel.histories.observe(viewLifecycleOwner) { turtles ->
+            turtles?.let { showRecyclerList(it) }
+        }
+        viewModel.isLoading.observe(viewLifecycleOwner){
+            binding.progressCircular.visibility = if (it) View.VISIBLE else View.GONE
+        }
+
+        return view
+    }
+
+    private fun showRecyclerList(turtles: List<Turtle>) {
+        val rvHistories = binding.rvHistories // Access RecyclerView from binding
+
+        val listHeroAdapter = HistoryAdapter(requireContext(), turtles)
+        rvHistories.adapter = listHeroAdapter
+
+        listHeroAdapter.setOnItemClickCallback(object : HistoryAdapter.OnItemClickCallback {
+            override fun onItemClicked(turtle: Turtle) {
+//                val intent = Intent(this@HistoryFragment, EnsiklopediaDetail::class.java)
+//                intent.putStringExtra("turtle_id", turtle_id)
+//                startActivity(intent)
+            }
+        })
     }
 
     companion object {
--- app/src/main/java/com/bangkit/turtlify/ui/history/HistoryViewModel.kt
+++ app/src/main/java/com/bangkit/turtlify/ui/history/HistoryViewModel.kt
@@ -1,7 +1,32 @@
 package com.bangkit.turtlify.ui.history
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bangkit.turtlify.data.database.repository.DatabaseRepository
+import com.bangkit.turtlify.data.database.repository.Turtle
+import kotlinx.coroutines.launch
 
 class HistoryViewModel : ViewModel(){
+    private val repository = DatabaseRepository()
+    private val _isLoading:MutableLiveData<Boolean> = MutableLiveData()
+    private val _histories:MutableLiveData<List<Turtle>?> = MutableLiveData()
 
+    val isLoading:LiveData<Boolean> = _isLoading
+    val histories: MutableLiveData<List<Turtle>?> = _histories
+
+
+    fun getHistories(){
+        viewModelScope.launch {
+            _isLoading.postValue(true)
+            try {
+                val histories = repository.getHistory()
+                _histories.postValue(histories)
+                _isLoading.postValue(false)
+            }catch (e: Exception){
+                _isLoading.postValue(false)
+            }
+        }
+    }
 }
